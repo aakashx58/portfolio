8f3dd3aaa021e7a020e10aeca0e75f3bc8ed2c52
aakashx58 <aakashrajbanshi58@gmail.com>
2023-11-13 15:25:33 +0545

    first commit

---
+++ lib/utils/about_utils.dart
@@ -0,0 +1,7 @@
+class AboutUtils {
+  static const String aboutMeHeadline =
+      'I\'m Aakash Rajbanshi, a Flutter developer and Tech enthausiast.';
+
+  static const String aboutMeDetail =
+      'I\'ve completed my +2 degree from Greenland International College and I am persuing by bachelors degree from Nihareeka College of Science and Technology as a B.Sc.CSIT student. Currently working as a freelancer.';
+}
---
+++ lib/utils/project_utils.dart
@@ -0,0 +1,26 @@
+class ProjectUtils {
+  static const List<String> banners = [
+    "assets/projects/chat.png",
+    "assets/projects/scan.png",
+  ];
+
+  static const List<String> icons = [
+    "assets/projects/chatapp.png",
+    "assets/projects/scanner.png",
+  ];
+
+  static const List<String> titles = [
+    "ChatApp",
+    "Scanner",
+  ];
+
+  static const List<String> description = [
+    "A unique chatapp created with the help of flutter.",
+    "A unique scanner app that converts image to text in real time.",
+  ];
+
+  static const List<String> links = [
+    "https://github.com/aakashx58/chatapp",
+    "https://github.com/aakashx58/scanner",
+  ];
+}
